35a1b1fd0ffdad1b1fe15227318a1f5c0de74796
Mert <mertturkmenoglu99@gmail.com>
2020-01-16 20:15:51 +0300

    Add bubble sort example

--- src/sketch.js
+++ src/sketch.js
@@ -1,24 +1,42 @@
-let r = 32;
 let arr;
 let minElement;
 let maxElement;
-let dy;
-let showNumbers;
 let numberCount = 50;
+let allArr;
+let k;
 
 function setup() {
     createCanvas(960, 640);
     background(0);
+    allArr = [];
 
     arr = randomArray(numberCount, 1, 50);
     calculateArray(arrayExtremums(arr));
-    showNumbers = false;
+    bubbleSort(arr, minElement, maxElement);
+    k = 0;
+    frameRate(60);
+}
+
+function draw() {
+    clear();
+    background(51);
+    drawArray(allArr[k++], minElement, maxElement);
+    if (k >= allArr.length)
+        noLoop();
+}
+
+function drawArray(a, min, max) {
+    let r = width / a.length;
+    for (let i = 0; i < a.length; i++) {
+        fill(200, 0, 200, 200);
+        let yy = map(a[i], min, max, r / 4, height);
+        rect(i * r, height - yy, r, yy);
+    }
 }
 
 function calculateArray(ext) {
     minElement = ext['min'];
     maxElement = ext['max'];
-    r = width / arr.length;
 }
 
 function arrayExtremums(a) {
@@ -48,19 +66,16 @@ function randomArray(n, min, max) {
     return a;
 }
 
-function draw() {
-    clear();
-    background(51);
-
-    for (let i = 0; i < arr.length; i++) {
-        fill(200, 0, 200, 200);
-        let yy = map(arr[i], minElement, maxElement, r / 4, height);
-        rect(i * r, height - yy, r, yy);
-
-        if (showNumbers) {
-            textSize(24);
-            fill(255);
-            text(arr[i], i*r + r/2, height - 24);
+function bubbleSort(a) {
+    for (let i = 0; i < a.length - 1; i++) {
+        for (let j = 0; j < a.length - i - 1; j++) {
+            if (a[j] > a[j + 1]) {
+                allArr.push(a.slice());
+                let tmp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = tmp;
+            }
         }
     }
+    allArr.push(a.slice());
 }
